f1bf891adb63f2aac34822418141a240bf19345e
mannysin <sinclair2610@gmail.com>
2018-12-17 14:08:34 -0500

    working on single fest page

--- src/components/SingleFestival.js
+++ src/components/SingleFestival.js
@@ -35,40 +35,38 @@ class SingleReview extends Component{
     fetchFestival = (id) =>{
         Axios.get(`http://localhost:4000/api/festival/${id}`)
         .then((responseFromApi)=>{
-            this.setState({oneFestival: responseFromApi.data.events.event})
-            console.log("here is a single festival", responseFromApi.data.events.event)
+            console.log(" <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< here is a single festival", responseFromApi.data.oneFestival)
+
+            console.log("getting 1 fest.....", id)
+            this.setState({oneFestival: responseFromApi.data.oneFestival})
         })    
         .catch((err)=>{
         })
     }
 
     showOneFestival = () => {
-        
-        if(this.state.oneFestival){
+       const oneFestival = this.state.oneFestival; 
+        if(oneFestival){
+            console.log(oneFestival)
         //     const allFestivals = this.state.allTheFestivals.filter((eachFestival)=>{
         //         return eachFestival.save()
         //     })
 
-            return this.state.oneFestival.map((eachFestival)=>{
-                // const festID = eachFestival.id 
-                // console.log ("here is each fest ID to push------>>>",festID)
-                // console.log ("here is each fest ID to push------>>>",eachFestival._id)
                 return(
-                    <div className="festIndex-container" key={eachFestival._id}>
-                    <h1> Review Details Page</h1>
-                    
-                    {eachFestival.image ? <img src={eachFestival.image.medium.url} alt={`${eachFestival.title}`} /> : eachFestival.image = <img src="http://54.163.73.103/configfiles/No_Image.png" alt={`${eachFestival.title}`}/>}
-                        <h3>{eachFestival.title}</h3>
-                        <h3>When: {eachFestival.start_time}</h3>
-                        <h3>Where: {eachFestival.city_name}, {eachFestival.region_name}</h3>
-                        <h3>Venue: {eachFestival.venue_name} <br/> {eachFestival.venue_address}</h3>
-                        <h6>Festival Details: {eachFestival.description}</h6>
-                    </div>
+                    <div className="festIndex-container">
+                         <h1>  Details Page</h1>
+                         {/* <img src ={oneFestival.images.image.medium.url}/>                        */}
+                            <h1>{oneFestival.title}'s details:</h1>
+                            <h2>When: {oneFestival.start_time}</h2>
+                            <h3>Where: {oneFestival.city}, {oneFestival.country}</h3>
+                            <h3>Venue: {oneFestival.venue_name} <br/> {oneFestival.venue_address}</h3>
+                            <h6>Festival Details: {oneFestival.description}</h6>
+                </div>
             )
             
-        })
+        }
     }       
-    }
+    // }
 
 
 
@@ -80,8 +78,6 @@ class SingleReview extends Component{
         return(
             <div>
                 {this.showOneFestival()}
-                {/* {this.showReviewDetails()} */}
-                <h1> YO YO YO YYO YO</h1>
                 <br />
                 <br />
                 <br />
@@ -105,19 +101,6 @@ class SingleReview extends Component{
 
                 <label for="venueInput"></label>
                 <input id="venueInput" name="venue" value={this.state.value}  className="display"></input>
-
-                <label for="ratingInpute"></label>
-                <input id="ratingInpute" name="averageRating" value={this.state.value} className="display"></input>
-
-                <label for="startInpute"></label>
-                <input id="startInpute" name="startDate" value={this.state.value}  className="display"></input>
-
-                <label for="endInpute"></label>
-                <input id="endInpute" name="endDate" value={this.state.value}  className="display"></input>
-
-                <label for="statusInpute"></label>
-                <input id="statusInpute" name="status" value={this.state.value}  className="display"></input>
-
 
                 <div>
                 <button type="submit" className="button">Submit a Review</button>
